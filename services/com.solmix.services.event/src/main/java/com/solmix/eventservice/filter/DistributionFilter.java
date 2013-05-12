ef6749627deaf22854a491e268d2232117170713
solmix <solmix.f@gmail.com>
2013-05-12 18:40:19 +0800

    import services

---
+++ services/com.solmix.services.event/src/main/java/com/solmix/eventservice/filter/DistributionFilter.java
@@ -0,0 +1,36 @@
+/*
+ * ========THE SOLMIX PROJECT=====================================
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.gnu.org/licenses/ 
+ * or see the FSF site: http://www.fsf.org. 
+ */
+package com.solmix.eventservice.filter;
+
+import org.osgi.service.event.Event;
+
+
+/**
+ * 
+ * @author solomon
+ * @version 110035  2011-10-1
+ */
+
+public class DistributionFilter
+{
+
+    public static void filte(Event event){
+        
+    }
+}
