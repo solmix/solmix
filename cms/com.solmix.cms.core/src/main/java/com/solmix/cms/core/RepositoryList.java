2d0e04eb04bc6f4d2e6b8713cfc68a645080d6b5
solmix <solmix.f@gmail.com>
2013-05-22 00:28:30 +0800

    cms core import

---
+++ cms/com.solmix.cms.core/src/main/java/com/solmix/cms/core/RepositoryList.java
@@ -0,0 +1,68 @@
+/*
+ * SOLMIX PROJECT
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.gnu.org/licenses/ 
+ * or see the FSF site: http://www.fsf.org. 
+ */
+
+package com.solmix.cms.core;
+
+import javax.jcr.LoginException;
+import javax.jcr.Repository;
+import javax.jcr.RepositoryException;
+import javax.jcr.Session;
+
+/**
+ * 
+ * @author Administrator
+ * @version $Id$ 2012-9-26
+ */
+
+public class RepositoryList
+{
+
+    Repository repository;
+
+    /**
+     * @return the repository
+     */
+    public Repository getRepository() {
+        return repository;
+    }
+
+    /**
+     * @param repository the repository to set
+     */
+    public void setRepository(Repository repository) {
+        this.repository = repository;
+    }
+
+    public void start() {
+        try {
+            Session ss = repository.login();
+            Repository repo = ss.getRepository();
+            ss.getWorkspace().getName();
+        } catch (LoginException e) {
+            e.printStackTrace();
+        } catch (RepositoryException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void stop() {
+
+    }
+}
