5b797e13ed6641e03af22fbe401b0f07619f5939
solmix <solmix.f@gmail.com>
2013-07-14 10:20:16 +0800

    0.3.x-SNAPSHOT 版本初始化

---
+++ services/org.solmix.services.event/src/main/java/org/solmix/eventservice/deliver/Rendezvous.java
@@ -0,0 +1,74 @@
+/*
+ * ========THE SOLMIX PROJECT=====================================
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.gnu.org/licenses/ 
+ * or see the FSF site: http://www.fsf.org. 
+ */
+package org.solmix.eventservice.deliver;
+
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * 
+ * @author solomon
+ * @version 110035 2011-10-2
+ */
+
+public class Rendezvous extends CyclicBarrier {
+	/** Flag for timeout handling. */
+	private volatile boolean timedout = false;
+
+	/**
+	 * Create a Barrier for the indicated number of parties, and the default Rotator function to run at each barrier
+	 * point.
+	 */
+	public Rendezvous() {
+		super(2);
+	}
+
+	/**
+	 * see {@link CyclicBarrier#barrier()}
+	 */
+	public void waitForRendezvous() {
+		if(timedout) {
+			// if we have timed out, we return immediately
+			return;
+		}
+		try {
+			this.await();
+		} catch(BrokenBarrierException ignore1) {
+		} catch(InterruptedException ignore2) {
+		}
+	}
+
+	/**
+	 * see {@link CyclicBarrier#attemptBarrier(long)}
+	 */
+	public void waitAttemptForRendezvous(final long timeout) throws TimeoutException {
+		try {
+			this.await(timeout, TimeUnit.MILLISECONDS);
+			// rest timeout.
+			this.reset();
+		} catch(BrokenBarrierException ignore1) {
+		} catch(TimeoutException te) {
+			timedout = true;
+			throw te;
+		} catch(InterruptedException ignore2) {
+		}
+	}
+}
---
+++ services/org.solmix.services.event/src/main/java/org/solmix/eventservice/filter/CachedTopicFilter.java
@@ -0,0 +1,89 @@
+/*
+ * ========THE SOLMIX PROJECT=====================================
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.gnu.org/licenses/ 
+ * or see the FSF site: http://www.fsf.org. 
+ */
+
+package org.solmix.eventservice.filter;
+
+import org.osgi.service.event.EventConstants;
+import org.solmix.eventservice.Cache;
+import org.solmix.eventservice.TopicFilter;
+
+/**
+ * 
+ * @author solomon
+ * @version 110035 2011-10-1
+ */
+
+public class CachedTopicFilter implements TopicFilter
+{
+
+    private Cache<String, String> cache;
+
+    private final char[] keyChars = EventConstants.EVENT_TOPIC.toCharArray();
+
+    private final char[] filterStart;
+
+    public CachedTopicFilter(final Cache<String, String> cache, boolean requireTopic)
+    {
+        if (null == cache) {
+            throw new NullPointerException("Cache may not be null");
+        }
+
+        this.cache = cache;
+
+        filterStart = ("(|" + ((requireTopic) ? "" : "(!(" + new String(keyChars) + "=*))") + "("
+            + new String(keyChars) + "=\\*)(" + new String(keyChars) + "=").toCharArray();
+    }
+
+    /**
+     * {@inheritDoc}
+     * 
+     * @see org.solmix.eventservice.TopicFilter#createFilter()
+     */
+    @Override
+    public String createFilter(String topic) {
+        String result = (String) cache.get(topic);
+
+        if (null == result) {
+            char[] topicChars = topic.toCharArray();
+
+            final StringBuffer filter = new StringBuffer(topicChars.length * topicChars.length);
+
+            filter.append(filterStart);
+
+            for (int i = 0; i < topicChars.length; i++) {
+                if ('/' == topicChars[i]) {
+                    filter.append('/').append('\\').append('*').append(')');
+
+                    filter.append('(').append(keyChars).append('=').append(topicChars, 0, i + 1);
+                } else {
+                    filter.append(topicChars[i]);
+                }
+            }
+
+            filter.append(')').append(')');
+
+            result = filter.toString();
+
+            cache.add(topic, result);
+        }
+
+        return result;
+    }
+
+}
