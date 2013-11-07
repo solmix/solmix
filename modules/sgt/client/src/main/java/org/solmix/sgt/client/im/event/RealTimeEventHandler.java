b907322b3d5817cd9c299579014bf4984223726c
solmix <solmix.f@gmail.com>
2013-11-08 00:05:47 +0800

    更新atmosphere 2.0,

--- modules/sgt/client/src/main/java/org/solmix/sgt/client/im/event/RealTimeEventHandler.java
+++
@@ -1,8 +0,0 @@
-package org.solmix.sgt.client.im.event;
-
-import com.google.gwt.event.shared.EventHandler;
-
-public interface RealTimeEventHandler extends EventHandler{
-
-	void onHandle(RealTimeEvent event);
-}
