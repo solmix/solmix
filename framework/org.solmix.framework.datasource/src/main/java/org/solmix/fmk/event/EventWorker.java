43a9ae8aaa47c49ec0b120234022f9380025605e
solmix <solmix.f@gmail.com>
2013-11-10 11:11:17 +0800

    增加EventWorker发送事项

--- framework/org.solmix.framework.datasource/src/main/java/org/solmix/fmk/event/EventWorker.java
+++ framework/org.solmix.framework.datasource/src/main/java/org/solmix/fmk/event/EventWorker.java
@@ -19,32 +19,54 @@
 
 package org.solmix.fmk.event;
 
+import org.osgi.framework.BundleContext;
+import org.solmix.api.context.SystemContext;
+import org.solmix.api.event.EventManager;
 import org.solmix.api.event.IEvent;
 import org.solmix.api.event.IValidationEvent;
 import org.solmix.api.event.IValidationEvent.Level;
 import org.solmix.api.event.IValidationEvent.OutType;
 import org.solmix.api.event.MonitorEventFactory;
 import org.solmix.api.exception.SlxException;
-import org.solmix.fmk.context.SlxContext;
 import org.solmix.fmk.datasource.ValidationEventFactory;
 
 /**
  * 
  * @author solmix.f@gmail.com
- * @version 110035 2012-11-30
+ * @version $Id$ 2013-11-9
  */
 
-public class EventUtils
+public class EventWorker
 {
 
-    static ValidationEventFactory veFactory;
+    private final SystemContext sc;
 
-    public final static IEvent createTimeMonitorEvent(long time, String msg) {
+    public EventWorker()
+    {
+        this(null);
+    }
+
+    public EventWorker(SystemContext sc)
+    {
+        this.sc = sc;
+    }
+
+    public EventManager getEventManager() {
+        if (sc != null) {
+            EventManager em = sc.getBean(EventManager.class);
+            if (em != null)
+                return em;
+        }
+        return new NullEventManager();
+
+    }
+
+    public final IEvent createTimeMonitorEvent(long time, String msg) {
         MonitorEventFactory factory;
         boolean destory = false;
-        if (SlxContext.getBundleContext() != null) {
+        if (sc != null && sc.getBean(BundleContext.class) != null) {
             destory = true;
-            factory = MonitorEventFactory.getInstance(SlxContext.getBundleContext());
+            factory = MonitorEventFactory.getInstance(sc.getBean(BundleContext.class));
         } else {
             factory = MonitorEventFactory.getDefault();
         }
@@ -54,23 +76,21 @@ public class EventUtils
         return e;
     }
 
-    public static void createAndFireTimeEvent(long time, String msg) {
-        SlxContext.getEventManager().postEvent(createTimeMonitorEvent(time, msg));
+    public final void createAndFireTimeEvent(long time, String msg) {
+        getEventManager().postEvent(createTimeMonitorEvent(time, msg));
     }
 
-    public final static IEvent createFieldValidationEvent(Level level, String msg) throws SlxException {
-        if (veFactory == null)
-            veFactory = ValidationEventFactory.instance();
+    public final IEvent createFieldValidationEvent(Level level, String msg) throws SlxException {
+        ValidationEventFactory veFactory = ValidationEventFactory.instance();
         veFactory.setType(ValidationEventFactory.TYPE_FIELD);
         IValidationEvent event = veFactory.create(level, msg);
         return event;
     }
 
-    public final static IEvent createDSValidationEvent(Level level, String msg, Throwable e) {
+    public final IEvent createDSValidationEvent(Level level, String msg, Throwable e) {
 
-        if (veFactory == null) {
-            veFactory = ValidationEventFactory.instance();
-        }
+        ValidationEventFactory veFactory = ValidationEventFactory.instance();
+        veFactory = ValidationEventFactory.instance();
         veFactory.setType(ValidationEventFactory.TYPE_DS);
         IValidationEvent event = null;
         try {
@@ -80,17 +100,11 @@ public class EventUtils
         return event;
     }
 
-    public final static void createAndFireFldValidateEvent(Level level, String msg) throws SlxException {
-        SlxContext.getEventManager().postEvent(createFieldValidationEvent(level, msg));
-        // handle the event out of box,With osgi handle by add osgi-event handler ,With spring used spring context event
-        // handler.
-        // LoggerFactory.getLogger(SlxLog.VALIDATION_LOGNAME).debug(msg);
+    public final void createAndFireFldValidateEvent(Level level, String msg) throws SlxException {
+        getEventManager().postEvent(createFieldValidationEvent(level, msg));
     }
 
-    public final static void createAndFireDSValidateEvent(Level level, String msg, Throwable e) {
-        SlxContext.getEventManager().postEvent(createDSValidationEvent(level, msg, e));
-        // handle the event out of box,With osgi handle by add osgi-event handler ,With spring used spring context event
-        // handler.
-        // LoggerFactory.getLogger(SlxLog.VALIDATION_LOGNAME).debug(msg);
+    public final void createAndFireDSValidateEvent(Level level, String msg, Throwable e) {
+        getEventManager().postEvent(createDSValidationEvent(level, msg, e));
     }
 }
---
+++ framework/org.solmix.framework.datasource/src/main/java/org/solmix/fmk/event/NullEventManager.java
@@ -0,0 +1,70 @@
+/*
+ * SOLMIX PROJECT
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.gnu.org/licenses/ 
+ * or see the FSF site: http://www.fsf.org. 
+ */
+package org.solmix.fmk.event;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.solmix.api.event.EventManager;
+import org.solmix.api.event.IEvent;
+
+
+/**
+ * 
+ * @author solmix.f@gmail.com
+ * @version $Id$  2013年11月8日
+ */
+
+public class NullEventManager implements EventManager
+{
+
+    
+    private static final Logger LOG = LoggerFactory.getLogger(NullEventManager.class);
+    /**
+     * {@inheritDoc}
+     * 
+     * @see org.solmix.api.event.EventManager#postEvent(org.solmix.api.event.IEvent)
+     */
+    @Override
+    public void postEvent(IEvent event) {
+        LOG.trace("postEvent():{}", event);
+
+    }
+
+    /**
+     * {@inheritDoc}
+     * 
+     * @see org.solmix.api.event.EventManager#sendEvent(org.solmix.api.event.IEvent)
+     */
+    @Override
+    public void sendEvent(IEvent event) {
+        LOG.trace("sendEvent():{}", event);
+
+    }
+
+    /**
+     * {@inheritDoc}
+     * 
+     * @see org.solmix.api.event.EventManager#getProvider()
+     */
+    @Override
+    public String getProvider() {
+        return "null manager setting,used this";
+    }
+
+}
