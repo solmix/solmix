451706354715fec91b6f746915e80a3b6b6fd99e
solmix <solmix.f@gmail.com>
2014-07-10 23:10:52 +0800

    solmix命令更新,添加下载mvn

--- tooling/commands/archive-install/src/main/java/org/solmix/command/karaf/FeatureDowndloadCommand.java
+++ tooling/commands/archive-install/src/main/java/org/solmix/command/karaf/FeatureDowndloadCommand.java
@@ -19,17 +19,8 @@
 
 package org.solmix.command.karaf;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.MalformedURLException;
 import java.net.URI;
-import java.net.URL;
-import java.net.URLConnection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -43,7 +34,6 @@ import org.apache.karaf.features.Repository;
 import org.apache.karaf.features.Resolver;
 import org.apache.karaf.shell.commands.Argument;
 import org.apache.karaf.shell.commands.Command;
-import org.apache.karaf.shell.console.OsgiCommandSupport;
 import org.osgi.framework.Constants;
 import org.osgi.framework.FrameworkUtil;
 import org.osgi.framework.ServiceReference;
@@ -56,11 +46,11 @@ import org.slf4j.LoggerFactory;
  * @author solmix.f@gmail.com
  * @version $Id$ 2013-12-12
  */
-@Command(scope = "solmix", name = "download", description = "downlaod the karaf features relative archives")
-public class ArchiveInstallCommand extends OsgiCommandSupport
+@Command(scope = "download", name = "feature", description = "downlaod the karaf features relative archives")
+public class FeatureDowndloadCommand extends AbstractDownloadCommand
 {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(ArchiveInstallCommand.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(FeatureDowndloadCommand.class);
 
     @Argument(index = 0, name = "feature", description = "The name and version of the features to install. A feature id looks like name/version. The version is optional.", required = true, multiValued = true)
     List<String> features;
@@ -147,58 +137,7 @@ public class ArchiveInstallCommand extends OsgiCommandSupport
         }
     }
 
-    private void downLoadFile(String location,String finalname)throws IOException{
-        File file = new File(finalname);
-        if (file.exists()) {
-            System.out.println("Ignoring bundle from \u001B[36m" + location + "\u001B[0m ");
-            return;
-        } else {
-            System.out.println("Downloading bundle from \u001B[33m" + location+"\u001B[0m");
-        }
-        InputStream is = null;
-        FileOutputStream fop = null;
-        File tmp = new File(finalname+".jtmp");
-        try {
-            URLConnection conn= new URL(location).openConnection();
-           if( conn.getContentLength()!=-1)
-            System.out.println("Downloading bundle size \u001B[33m" + conn.getContentLength()+"Byte\u001B[0m");
-            is = new BufferedInputStream(conn.getInputStream());
-            if (!tmp.exists()) {
-                File parentFile = tmp.getParentFile();
-                if (parentFile != null) {
-                    parentFile.mkdirs();
-                }
-                tmp.createNewFile();
-            }else{
-                tmp.delete();
-                tmp.createNewFile();
-            }
-
-            fop = new FileOutputStream(tmp);
-
-            int bytesRead = 0;
-            byte[] buffer = new byte[1024];
-
-            while ((bytesRead = is.read(buffer)) != -1) {
-                fop.write(buffer, 0, bytesRead);
-            }
-            System.out.println("Saved bundle at \u001B[32m" + file.getPath()+"\u001B[0m");
-        } catch (RuntimeException e) {
-            LOGGER.error(e.getMessage());
-            throw e;
-        } catch (MalformedURLException e) {
-            LOGGER.error(e.getMessage());
-            throw e;
-        } finally {
-            if (is != null)
-                is.close();
-            if (fop != null) {
-                fop.flush();
-                fop.close();
-            }
-        }
-        renameFile(tmp,file);
-    }
+   
     /**
      * @param bInfo
      * @throws IOException 
@@ -209,63 +148,8 @@ public class ArchiveInstallCommand extends OsgiCommandSupport
         downLoadFile(location,finalname);
     }
     
-    private void renameFile(File src,File target) throws IOException{
-        BufferedInputStream inBuffer=null;
-        BufferedOutputStream outBuffer=null;
-        try {
-            inBuffer= new BufferedInputStream(new FileInputStream(src));
-             outBuffer=new BufferedOutputStream(new FileOutputStream(target));
-             int bytesRead = 0;
-             byte[] buffer = new byte[1024];
-
-             while ((bytesRead = inBuffer.read(buffer)) != -1) {
-                 outBuffer.write(buffer, 0, bytesRead);
-             }
-             outBuffer.flush();
-        } catch (Exception e) {
-            LOGGER.error(e.getMessage());
-        }finally {
-            if (inBuffer != null)
-                inBuffer.close();
-            if (outBuffer != null) {
-                outBuffer.flush();
-                outBuffer.close();
-            }
-        }
-        src.delete();
-        
-    }
     
-    /**
-     * @param location
-     * @return
-     */
-    private String getDownLoadedFile(String location) {
-        String repository = System.getProperty("karaf.default.repository");
-        String base = System.getProperty("karaf.home");
-        String protocol = location.substring(0, location.indexOf(":") );
-        String path = location.substring(location.indexOf(":") + 1);
-        StringBuilder sb = new StringBuilder().append(base).append(File.separatorChar).append(repository).append(File.separatorChar);
-        if ("mvn".equals(protocol)) {
-            String[] strs = path.split("/");
-            String groupId = strs[0].replace('.',File.separatorChar);
-            String artificationId = strs[1];
-            String version = strs[2];
-            String type = "jar";
-            if (strs.length > 3)
-                type = strs[3];
-            String classifier = null;
-            if (strs.length > 4)
-                classifier = strs[4];
-            StringBuffer file=new StringBuffer().append(artificationId).append('-').append(version);
-                if(classifier!=null)
-                    file.append('-').append(classifier);
-                file.append('.').append(type);
-            sb.append(groupId).append(File.separatorChar).append(artificationId).append(File.separatorChar).append(version).append(File.separatorChar).append(file);
-        }
-
-        return sb.toString();
-    }
+   
 
     /**
      * @param location
