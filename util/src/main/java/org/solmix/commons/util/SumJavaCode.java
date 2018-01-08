36ad294ef04d3e9ccc8c498219c11b2bf012b79e
homopm <homopm@163.com>
2018-01-08 20:20:12 +0800

    aaa

--- util/src/main/java/org/solmix/commons/util/SumJavaCode.java
+++ util/src/main/java/org/solmix/commons/util/SumJavaCode.java
@@ -34,7 +34,7 @@ public class SumJavaCode
         SumJavaCode sjc = new SumJavaCode();
         // File f = new File(System.getProperty("user.dir") + System.getProperty("file.separator") + "src");
         // File f = new File("M:\\workspace\\platform\\core\\trunk\\solmix-web\\solmix-app\\src");
-        File f = new File("/home/solmix/o/example");
+        File f = new File("/home/solmix/o/gits/homo");
         System.out.println(f.getName());
         sjc.treeFile(f);
         System.out.println("文件数目:"+sjc.getFileCount()+" 总代码行数:"+(sjc.getWhiteLines()+sjc.getNormalLines()+sjc.getCommentLines()));
