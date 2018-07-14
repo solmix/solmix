22ba79b09486b5b3e05ee6a5c531014bfc715824
solmix <solmix@localhost>
2018-07-14 09:45:26 +0800

    a

---
+++ util/src/test/java/org/solmix/commons/util/NetUtilsTest.java
@@ -0,0 +1,14 @@
+package org.solmix.commons.util;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class NetUtilsTest {
+
+	  @Test
+	    public void isValidAddress() throws Exception {
+	        boolean valid=NetUtils.isValidAddress("192.168.0.100");
+
+	        Assert.assertTrue(valid);
+	    }
+}
